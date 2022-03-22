613153d7ba14d55b9d96c00df5530dcdca676431
103SABAH_BATUHAN <103SABAH_BATUHAN@DESKTOP-HNE43R2>
2022-03-22 13:33:36 +0300

    Add project files.

---
+++ MovieMeet_EL/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieMeet_EL.Models
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public int RunTime { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+        public string Director { get; set; }
+        public string Cast1 { get; set; }
+        public string Cast2 { get; set; }
+        public string Cast3 { get; set; }
+        public string Cast4 { get; set; }
+        public string IMDBLink { get; set; }
+    }
+}
